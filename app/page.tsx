165111c5b90b7ce1a12aae982ba376d1e5596cdf
TomasKliner <50572595+TomasKliner@users.noreply.github.com>
2024-02-24 11:06:45 +0000

    only darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,17 @@ export default function Home() {
     `function add(a, b) {\n  return a + b;\n}`
   );
   return (
-    <div className="flex w-full min-h-screen dark:bg-neutral-800">
+    <div className="dark flex w-full min-h-screen bg-neutral-800">
       <section id="sidebar"
-        className="w-64 dark:bg-neutral-600 rounded-r-xl p-4">
-        <h1 className="dark:text-white font-black drop-shadow dark:bg-neutral-800 p-4 rounded-xl text-center
+        className="w-64 bg-neutral-600 rounded-r-xl p-4">
+        <h1 className="text-white font-black drop-shadow bg-neutral-800 p-4 rounded-xl text-center
           ">SHORTCUTS TRAINER</h1>
       </section>
       <main id="editor"
         className="flex-grow h-full flex flex-col">
         <div style={{ backgroundColor: "#111111" }}
-          className="h-48 w-full flex justify-center items-center">
-          <div className=" h-32 w-4/5 p-4 rounded-2xl shadow dark:bg-neutral-700  ">
+          className="h-48 w-full flex justify-center items-center text-white">
+          <div className=" h-32 w-4/5 p-4 rounded-2xl shadow bg-neutral-700  ">
             <h2 className="font-bold text-xl">Instructions</h2>
             <div  className="flex gap-2">
               <p>do this</p>
